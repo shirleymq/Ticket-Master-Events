2605eaa68589242dce5fef2f3e985ca4172919cb
Shirley Quelali <shirley.quelali@gmail.com>
2024-06-11 23:44:41 -0400

    demo of forwardRef

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,20 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import eventsJSON from "../data/events.json";
 
 const useEventsData = () => {
-  const data = useRef([]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
     setTimeout(() => {
-      data.current = eventsJSON;
+      try {
+        setData(eventsJSON);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+      }
     }, 4000);
     //Load API call
   }, []);
 
-  console.log(data.current);
+  console.log(data);
 
   return {
-    events: data.current?._embedded?.events || [],
+    events: data?._embedded?.events || [],
+    isLoading,
+    error,
   };
 };
 
