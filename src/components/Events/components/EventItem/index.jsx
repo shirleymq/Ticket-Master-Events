e11d0c41c36ed9a45285ece5ecc48b942187a590
Shirley Quelali <shirley.quelali@gmail.com>
2024-06-12 23:50:32 -0400

    styles of Event item

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,3 +1,4 @@
+import "./styles.css";
 const EventItem = ({ info, id, name, image, onEventClick }) => {
   const handleSeeMoreClick = (evt) => {
     evt.stopPropagation();
@@ -5,11 +6,18 @@ const EventItem = ({ info, id, name, image, onEventClick }) => {
   };
 
   return (
-    <div onClick={() => console.log("padre clickeado")}>
+    <div
+      onClick={() => console.log("padre clickeado")}
+      className="event-item-container"
+    >
       <img src={image} alt={name} width={200} height={200} />
-      <h4>{name}</h4>
-      <p>{info}</p>
-      <button onClick={handleSeeMoreClick}>Ver mas</button>
+      <div className="event-info-container">
+        <h4 className="event-name">{name}</h4>
+        <p className="event-info">{info}</p>
+        <button onClick={handleSeeMoreClick} className="see-more-btn">
+          Ver mas
+        </button>
+      </div>
     </div>
   );
 };
