6a2479e55f6492a68962aa28863ec6932eb4c5af
Shirley Quelali <shirley.quelali@gmail.com>
2024-07-05 03:14:45 -0400

    fetch at event detail

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
   const { eventId } = useParams();
-  console.log(eventId);
+  const [eventData, setEventData] = useState({});
+  const [error, setError] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchEventsData = async () => {
+      try {
+        const response = await fetch(
+          `https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=tOs157YEu5aDh5TjjoMvArjHh1KChoiS`
+        );
+        const data = await response.json();
+        setEventData(data);
+        setIsLoading(false);
+      } catch (error) {
+        setEventData({});
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchEventsData();
+  }, []);
+
+  console.log(eventData);
   return <div> Detail</div>;
 };
 export default Detail;
